2020f4f07687acc2d9292887ad32833631204036
Wilson <zenfonlee@gmail.com>
2024-07-17 15:57:01 +0800

    搜尋功能修改完成

--- src/app/manage/accountbook/accountbook.module.ts
+++ src/app/manage/accountbook/accountbook.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AccountbookRoutingModule } from './accountbook-routing.module';
 import { AccountbookComponent } from './accountbook.component';
 import { DatePipe } from '@angular/common';
-import { AccountbookFunctionComponent } from './accountbook-function/accountbook-function.component';
 
 
 @NgModule({
   declarations: [
     AccountbookComponent,
-    AccountbookFunctionComponent
   ],
   imports: [
     CommonModule,
